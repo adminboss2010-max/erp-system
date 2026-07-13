1cf275c192c439bcb1fd98835009b3b90137f5c4
adminboss2010-max <adminboss2010@gmail.com>
2026-07-13 22:38:14 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// ضع هنا رابط الـ Web App بعد نشر Code.gs (Deploy > New deployment > Web app)
+// شكل الرابط: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
+window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxGYnh-DmAlDyF1nL-K4DwO4BpXseRrfcSYCw3K8-nEfT3mpJwP-kAbQZhQGY2cGFM/exec";
