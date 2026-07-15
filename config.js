f7502a004b6b3c708b6aec0ec40908c2acff80aa
adminboss2010-max <adminboss2010@gmail.com>
2026-07-15 08:43:54 +0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// ضع هنا رابط الـ Web App بعد نشر Code.gs (Deploy > New deployment > Web app)
-// شكل الرابط: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
-window.APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw9Y9a6_b-01md17uq7XV8vSpI0wdWridCrpJJ_pC2L4DHDNj6jIEJrYwvF6bkjbw/exec";
